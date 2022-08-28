433f549e5f4ffaa5467784769c422e08460d19ca
indexsale <112284961+indexsale@users.noreply.github.com>
2022-08-27 21:16:21 -0300

    Update index.tsx

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -25,7 +25,7 @@ const Menu = (props) => {
       setLang={setSelectedLanguage}
       cakePriceUsd={cakePriceUsd.toNumber()}
       links={config}
-      priceLink="https://www.coingecko.com/en/coins/CryptoFan"
+      priceLink="https://www.coingecko.com/en/coins/indexsale"
       {...props}
     />
   )
