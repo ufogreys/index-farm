451bf8c0086be376bb538b2dfa4d884e88d8b831
indexsale <112284961+indexsale@users.noreply.github.com>
2022-08-27 21:13:30 -0300

    Update ApyCalculatorModal.tsx

--- src/views/Farms/components/FarmCard/ApyCalculatorModal.tsx
+++ src/views/Farms/components/FarmCard/ApyCalculatorModal.tsx
@@ -67,7 +67,7 @@ const ApyCalculatorModal: React.FC<ApyCalculatorModalProps> = ({
         </GridItem>
         <GridItem>
           <Text fontSize="12px" bold color="textSubtle" textTransform="uppercase" mb="20px">
-            {TranslateString(999, 'CryptoFan per $1000')}
+            {TranslateString(999, 'In-Dex Sale per $1000')}
           </Text>
         </GridItem>
         {/* 1 day row */}
